1c277e9eadd8361cab123beaae0ad2c851b460be
Andrey Yakushin <a.yakushin@adguard.com>
2026-05-19 02:03:42 -0700

    Remove duplicated vpn state field in UI

--- platform/testapp/lib/main.dart
+++ platform/testapp/lib/main.dart
@@ -345,10 +345,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             const SizedBox(height: 10.0),
-            Text(
-              vpnStateWatcher.state.name,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
             const SizedBox(height: 10.0),
             Text(
               'VPN State: ${vpnStateWatcher.state.name}',
